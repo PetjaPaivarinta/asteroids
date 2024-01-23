6a2fe80b363df759e0816e83a536897e6b482b31
PetjaPaivarinta <petjapaivarinta1@gmail.com>
2024-01-23 09:23:24 +0200

    first commit

---
+++ Assets/Scripts/Score.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Score : MonoBehaviour
+{
+
+    public static Score instance;
+
+    public Text scoreText;
+    public Text highscoreText;
+
+    int score = 0;
+    int highscore = 0;
+
+    private void Awake()
+    {
+        instance = this;
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        highscore = PlayerPrefs.GetInt("highscore", 0);
+        scoreText.text = score.ToString() + " Points";
+        highscoreText.text = "Highscore " + highscore.ToString();
+        Debug.Log(highscore);
+    }
+
+    public void AddPoint()
+    {
+        score += 10;
+        scoreText.text = score.ToString() + " Points";
+        if (highscore < score)
+            PlayerPrefs.SetInt("highscore", score);
+        
+    }
+}
